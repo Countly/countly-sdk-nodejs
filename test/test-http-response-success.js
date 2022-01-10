4a07b3fdf5e22d6c8cb98d773623247644d70778
turtledreams <deniztaygunerten@gmail.com>
2022-01-10 16:05:07 +0300

    latest fixes

--- test/test-http-response-success.js
+++ test/test-http-response-success.js
@@ -5,45 +5,53 @@ var cc = require("../lib/countly-common");
 //unit tests for isResponseValid
 describe("Response success suite", ()=>{
     it("Check if correct response parameters returns true", ()=>{
-        var res = {"statusCode": 200};
-        var str = {"result": "Success"};
-        var result = cc.isResponseValid(res, str);
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(200, str);
         assert.ok(result);
     });
     it("Check if wrong response that includes result in it returns false", ()=>{
-        var res = {"statusCode": 200};
-        var str = {"endResult": "Success"};
-        var result = cc.isResponseValid(res, str);
+        var str = '{"endResult": "Success"}';
+        var result = cc.isResponseValid(200, str);
+        assert.equal(result, false);
+    });
+    it("Check if wrong response that includes result in it returns false", ()=>{
+        var str = '{"end": "Success"}';
+        var result = cc.isResponseValid(200, str);
         assert.equal(result, false);
     });
     it("Check if wrong statusCode returns false", ()=>{
-        var res = {"statusCode": 400};
-        var str = {"result": "Success"};
-        var result = cc.isResponseValid(res, str);
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(400, str);
+        assert.equal(result, false);
+    });
+    it("Check if wrong statusCode returns false", ()=>{
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(100, str);
+        assert.equal(result, false);
+    });
+    it("Check if wrong statusCode returns false", ()=>{
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(300, str);
         assert.equal(result, false);
     });
     it("Check if non Success value at result field returns true", ()=>{
-        var res = {"statusCode": 200};
-        var str = {"result": "Sth"};
-        var result = cc.isResponseValid(res, str);
+        var str = '{"result": "Sth"}';
+        var result = cc.isResponseValid(200, str);
         assert.equal(result, true);
     });
     it("Check if can parse JSON and returns true", ()=>{
-        var res = {"statusCode": 200};
         var str = '{"result": "Success"}';
-        var result = cc.isResponseValid(res, str);
+        var result = cc.isResponseValid(200, str);
         assert.equal(result, true);
     });
     it("Check if there is no statusCode it returns false", ()=>{
-        var res = {"a": 200};
-        var str = {"result": "Success"};
-        var result = cc.isResponseValid(res, str);
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid({}.a, str);
         assert.equal(result, false);
     });
     it("Check if just string/ non object returns false", ()=>{
-        var res = {"statusCode": 200};
         var str = "RESULT";
-        var result = cc.isResponseValid(res, str);
+        var result = cc.isResponseValid(200, str);
         assert.equal(result, false);
     });
     it("Check if empty response returns false", ()=>{
