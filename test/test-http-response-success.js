b7bb1bc86fa254ed6030073444666e9dcb7be21a
turtledreams <deniztaygunerten@gmail.com>
2022-01-10 11:34:25 +0300

    tests added

---
+++ test/test-http-response-success.js
@@ -0,0 +1,49 @@
+/* global describe, it, */
+var assert = require("assert");
+var cc = require("../lib/countly-common");
+
+//unit tests for isResponseValid
+describe("Response success suite", ()=>{
+    it("Check if correct response parameters returns true", ()=>{
+        var res = {"statusCode": 200};
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(res, str);
+        assert.ok(result);
+    });
+    it("Check if wrong response that includes result in it returns false", ()=>{
+        var res = {"statusCode": 200};
+        var str = '{"endResult": "Success"}';
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, false);
+    });
+    it("Check if wrong statusCode returns false", ()=>{
+        var res = {"statusCode": 400};
+        var str = '{"result": "Success"}';
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, false);
+    });
+    it("Check if Fail result with result field existing returns true", ()=>{
+        var res = {"statusCode": 200};
+        var str = '{"result": "Fail"}';
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, true);
+    });
+    it("Check if non string str parsing fails it returns false", ()=>{
+        var res = {"statusCode": 200};
+        var str = {"result": "Success"};
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, false);
+    });
+    it("Check if there is no statusCode returns false", ()=>{
+        var res = {"a": 200};
+        var str = {"result": "Success"};
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, false);
+    });
+    it("Check if empty response returns false", ()=>{
+        var res = {};
+        var str = "";
+        var result = cc.isResponseValid(res, str);
+        assert.equal(result, false);
+    });
+});
