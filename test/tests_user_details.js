1ee347c1d902e3ace2e3d8fb858777b62d31b168
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-12-02 14:52:08 +0300

    feat: legacy and new usage tests

--- test/tests_user_details.js
+++ test/tests_user_details.js
@@ -8,14 +8,50 @@ describe("User details tests", () => {
     beforeEach(async() => {
         await hp.clearStorage();
     });
-    it("Record and validate all user details", (done) => {
+    it("Record and validate all user details - Legacy", (done) => {
         Countly.init({
             app_key: "YOUR_APP_KEY",
             url: "https://try.count.ly",
         });
         var userDetailObj = testUtils.getUserDetailsObj();
         Countly.user_details(userDetailObj);
-        // read event queue
+        // read request queue
+        setTimeout(() => {
+            var req = hp.readRequestQueue()[0];
+            const actualUserDetails = req.user_details;
+            const isValid = hp.validateUserDetails(actualUserDetails, userDetailObj);
+            assert.equal(true, isValid);
+            done();
+        }, hp.sWait);
+    });
+
+    it("Record and validate all user details", (done) => {
+        Countly.init({
+            app_key: "YOUR_APP_KEY",
+            url: "https://try.count.ly",
+        });
+        var userDetailObj = testUtils.getUserDetailsObj();
+        Countly.userProfile.set_properties(userDetailObj);
+        Countly.userProfile.save();
+        // read request queue
+        setTimeout(() => {
+            var req = hp.readRequestQueue()[0];
+            const actualUserDetails = req.user_details;
+            const isValid = hp.validateUserDetails(actualUserDetails, userDetailObj);
+            assert.equal(true, isValid);
+            done();
+        }, hp.sWait);
+    });
+
+    it("Record and validate all user details", (done) => {
+        Countly.init({
+            app_key: "YOUR_APP_KEY",
+            url: "https://try.count.ly",
+        });
+        var userDetailObj = testUtils.getUserDetailsObj();
+        Countly.userProfile.set_properties(userDetailObj);
+        Countly.userProfile.save();
+        // read request queue
         setTimeout(() => {
             var req = hp.readRequestQueue()[0];
             const actualUserDetails = req.user_details;
